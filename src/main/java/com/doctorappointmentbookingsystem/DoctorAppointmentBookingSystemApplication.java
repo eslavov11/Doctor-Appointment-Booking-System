4c355721ddb527275e1c27a35537d1048f1053b8
Edward Slavov <e.slavov11@gmail.com>
2017-04-03 22:59:41 +0300

    created spring boot project

---
+++ src/main/java/com/doctorappointmentbookingsystem/DoctorAppointmentBookingSystemApplication.java
@@ -0,0 +1,12 @@
+package com.doctorappointmentbookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorAppointmentBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorAppointmentBookingSystemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/doctorappointmentbookingsystem/DoctorAppointmentBookingSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.doctorappointmentbookingsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DoctorAppointmentBookingSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
