39065b3aad2b258e459185895ecad40102156a25
Edward Slavov <e.slavov11@gmail.com>
2017-04-06 23:02:41 +0300

    resources libs - bootstrap, jquery and other config files added

--- src/main/java/com/doctorAppointmentBookingSystem/DoctorAppointmentBookingSystemApplication.java
+++ src/main/java/com/doctorAppointmentBookingSystem/DoctorAppointmentBookingSystemApplication.java
@@ -1,12 +1,19 @@
 package com.doctorAppointmentBookingSystem;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DoctorAppointmentBookingSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DoctorAppointmentBookingSystemApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper getModelMapper(){
+		return new ModelMapper();
 	}
 }
